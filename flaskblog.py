8c6df1424883aea313ed2df29b4cb15294513bdb
Timur Salomatshoev <salomatshoev.ta@phystech.edu>
2022-08-05 07:44:20 +0300

    set SQLALCHEMY_TRACK_MODIFICATIONS to false

--- flaskblog.py
+++ flaskblog.py
@@ -7,6 +7,7 @@ from datetime import datetime
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '86d2c0ff1ef6ad4a5112163f97daaff9'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
