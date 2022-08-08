da0b1fe1da6c66c985da3114b788cb978f464d04
Timur Salomatshoev <salomatshoev.ta@phystech.edu>
2022-08-08 09:44:27 +0300

    add delete_post route

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -112,7 +112,7 @@ def post(post_id):
     return render_template('post.html', title=post.title, post=post)
 
 
-app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'] )
 @login_required
 def update_post(post_id):
     post = Post.query.get_or_404(post_id)
@@ -130,3 +130,15 @@ def update_post(post_id):
         form.content.data = post.content
     return render_template('create_post.html', title='Update Post',
                            form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
